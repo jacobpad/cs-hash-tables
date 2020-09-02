36875dceccaa0a77626b6493580dd8c67b340c76
jacobpad <jacobpad@gmail.com>
2020-09-02 14:21:03 -0700

    MVP no_dups.py

--- applications/04_no_dups/no_dups.py
+++ applications/04_no_dups/no_dups.py
@@ -1,6 +1,27 @@
 def no_dups(s):
-    # Your code here
+    webster = {}
 
+    # Edge case (see the first test)
+    if s == "":
+        return ""
+
+    # Split on spaces
+    s = s.split(" ")
+
+    # Add to webster only the words that don't exist in webster
+    # (NO DUPLICATES)
+    for i in s:
+        if i not in webster:
+            webster[i] = i
+
+    # Get just the words from the dictionary
+    lst = list(webster.keys())
+
+    # Make into a string seperated by spaces
+    s = " ".join(lst)
+
+    # Fulfill MVP
+    return s
 
 
 if __name__ == "__main__":
